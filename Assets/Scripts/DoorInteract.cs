1365e1e29b5ec8febaaf1c256c3ffb0ffb6b171b
Raheel21 <moh13357822@students.solihull.ac.uk>
2018-12-04 13:02:55 +0000

    new  scripts added also need to fix door issue

---
+++ Assets/Scripts/DoorInteract.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteract : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            Ray mouseRay = GetComponentInChildren<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo; 
+            if (Physics.Raycast(mouseRay, out hitInfo))
+            {
+
+                Dooropen door = hitInfo.transform.GetComponent<Dooropen>();
+                if (door)
+                {
+
+                    door.enabled = true; 
+                }
+
+            }
+
+        }
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
