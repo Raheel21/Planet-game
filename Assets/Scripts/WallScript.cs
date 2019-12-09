4cbd5b9f20d5d9b368d75ed588ec96545de82d05
Raheel21 <45422828+Raheel21@users.noreply.github.com>
2019-12-09 11:58:07 +0000

    added temporary portal

---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+
+            Destroy(other.gameObject);
+            Debug.Log("Object Destroyed"); 
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    
+}
