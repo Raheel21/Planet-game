874ab8d47788d0f264dbb7bd996d47ee2019b929
Raheel21 <45422828+Raheel21@users.noreply.github.com>
2019-12-09 14:11:14 +0000

    Portal Script added

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+
+            DontDestroyOnLoad(other.gameObject);
+            SceneManager.LoadScene("Level"); 
+            Debug.Log("Object Teleported");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
