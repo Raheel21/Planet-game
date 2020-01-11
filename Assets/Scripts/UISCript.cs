ccf735e7a0402d37341a5baea01676b461638a27
Raheel21 <moh13357822@students.solihull.ac.uk>
2020-01-11 21:25:30 +0000

    added UI health

---
+++ Assets/Scripts/UISCript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UISCript : MonoBehaviour
+{
+
+    public Health HealthScript;  
+    public Text HealthText;
+    public Slider HealthBar; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBar.maxValue = HealthScript.GetMaxHealth();
+        HealthBar.value = HealthScript.GetHealth();
+        HealthText.text = "Health" + HealthScript.GetHealth(); 
+        
+
+
+
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
