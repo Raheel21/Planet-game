78ef32cf147b73a426b4696b9668dec744449681
Raheel21 <45422828+Raheel21@users.noreply.github.com>
2020-01-13 15:18:56 +0000

    changed terrain

--- Assets/Scripts/UISCript.cs
+++ Assets/Scripts/UISCript.cs
@@ -8,23 +8,22 @@ public class UISCript : MonoBehaviour
 
     public Health healthScript;  
     public Text healthText;
-    public Slider healthBar; 
+    public Slider healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         healthBar.maxValue = healthScript.GetMaxHealth();
         healthBar.value = healthScript.GetHealth();
-        healthText.text = "Health" + healthScript.GetHealth(); 
-        
+        healthText.text = "Health" + healthScript.GetHealth();
 
-
-
-
-
-
-        
     }
+
+
+
+
+        
+    
 
     // Update is called once per frame
     void Update()
