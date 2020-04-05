b07c896e60139c6ee73f773b0829c50488b460a9
Raheel21 <moh13357822@students.solihull.ac.uk>
2020-04-05 23:56:35 +0100

    added comments to my health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,60 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-
-    [SerializeField] int maximumHealth = 100;
+   //Both my max health and current health are serialized so that they are visible in the unity inspector 
+   //And i can adjust both my health status from within the Unity inspector without having to change it within my script    
+    [SerializeField] int maximumHealth = 100; 
     [SerializeField] int currentHealth = 100;
 
     // Use this for initialization
     void Start()
     {
-
-        currentHealth = maximumHealth;
-
+        currentHealth = maximumHealth; //This is setting the current health to the max health 
     }
 
-    public bool IsDead { get { return currentHealth <= 0; } }
+    //Below i use properties to get back my players current and max health and check the if the health is less than or equal to 0
+    public bool IsDead { get { return currentHealth <= 0; } } 
 
+    //Below would be for getting back the players current health
     public int GetHealth()
     {
-
         return currentHealth;
-
     }
 
+    //Below would be used to get back the players max health 
     public int GetMaxHealth()
     {
-
         return maximumHealth;
-
     }
 
+    //Below is my damage function which is public so it can be used by my other scripts
+    //Allows integer type as damage value which is then taken away from the players current health
+    //If player current health is equal to or below 0 the the cursor is unlocked and visible along with the player game object gets destroyed
+    //Along with a message gets printed to know the player has been destroyed and then the lose scene is loaded
     public void Damage(int damageValue)
     {
         currentHealth -= damageValue;
-
         if (currentHealth <= 0)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             Destroy(gameObject);
             Debug.Log("Player Destroyed");
-            SceneManager.LoadScene("Lose Scene");
-            
-
+            SceneManager.LoadScene("Lose Scene");            
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (currentHealth > 100)//if player health is more than 100
         {
-
-            currentHealth = 100; //Health is set at 100 
-
+            currentHealth = 100; //Health is set at 100 so player cannot have more then 100 health  
         }
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -11,7 +11,7 @@ public class Shooting : MonoBehaviour {
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        //laser = transform.GetChild(4).GetChild(1).gameObject.GetComponent<MeshRenderer>();
+        laser = transform.GetChild(4).GetChild(1).gameObject.GetComponent<MeshRenderer>();
         //audioSrc = GetComponent<AudioSource>();
 
         
